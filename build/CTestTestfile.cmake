f2042023a1fa58b6ab4846078b6419c88957f9fa
dukakbey <sezginceyln@gmail.com>
2022-09-21 01:19:10 +0300

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/sceylan/source/repos/git/Qt
+# Build directory: C:/Users/sceylan/source/repos/git/Qt/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    std::cout<<"hello git"<<std::endl;
+    system("pause");
+    return 0;
+
+}
