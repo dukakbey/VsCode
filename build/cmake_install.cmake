caf510bcb2e945227fc1150cb5b6b6bcd3767586
dukakbey <sezginceyln@gmail.com>
2022-09-21 02:09:48 +0300

    "denemeler devam"

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files/Qt_first")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files/sc_qt")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
-
+#include <string>
+using namespace std;
 int main()
 {
-    std::cout<<"hello git"<<std::endl;
-    std::cout<<"hello sc"<<std::endl;
+    string username;
+    cin>>username;
+    cout<<"hello "<<username<<endl;
+    cout<<"are you ready for the game!!"<<std::endl;
+    cout << "are you ready for the game!!" << std::endl;
     system("pause");
     return 0;
 
