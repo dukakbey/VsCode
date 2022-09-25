049fbc20d22d9267627bcc0df77615d79508d301
dukakbey <sezginceyln@gmail.com>
2022-09-25 23:14:46 +0300

    prepare for cmake

--- src/hello.cpp
+++ src/hello.cpp
@@ -6,7 +6,7 @@ int main()
     string username;
     cin>>username;
     cout<<"hello "<<username<<endl;
-    cout<<"are you ready for the game!!"<<std::endl;
+    cout<<"are you ready for the euroleauge game!!"<<std::endl;
     cout << "are you ready for the game!!" << std::endl;
     system("pause");
     return 0;
